34b4ab16e3a1f20e9ab269235e5ac44c4f6876b4
Leandro <leandrolopez131982@gmail.com>
2023-08-01 05:58:07 -0300

    install mantine

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { suma } from "init-npm-por-lean-parte_tres";
- import { multiplicar } from "multiplicar";
+import { suma } from "init-npm-por-lean-parte_tres";/**/
+import { multiplicar } from "multiplicar"; 
+import { potencia } from "potencia"; 
 
 
-console.log(multiplicar(2, 3)); 
-console.log(suma(2, 3)) 
+
+console.log(multiplicar(8, 3)); 
+console.log(suma(8, 3)); /* */
+console.log(potencia(8, 3));
